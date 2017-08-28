4dbab2fffcbc6558b47cb904192c46bb7f4a84cf
mc4tech <chavezmeguel@yahoo.com>
2017-08-28 14:58:29 -0400

    api in shopping .js

--- models/shopping.js
+++ models/shopping.js
@@ -2,7 +2,7 @@
 var http = require("http");
 var authKey = "uid6676-39776974-43";
 var methodName = "brands";
-var url = "http://api.shopstyle.com/api/v2/brands?pid=" + authKey + "&format=json";
+var url = "http://api.shopstyle.com/api/v2/products?pid=" + authKey + "&fts=men&offset=0&limit=10";
 
 // get is a simple wrapper for request()
 // which sets the http method to GET
@@ -20,12 +20,18 @@ var request = http.get(url, function (response) {
     response.on("end", function (err) {
         // finished transferring data
         // dump the raw data
-        console.log(buffer);
+        // console.log(buffer);
         console.log("\n");
         data = JSON.parse(buffer);
         // route = data.routes[0];
-
-        console.log("data");
-        console.log(data);
+        // for(var i = 0; i < data.products.length; i++) {
+        //     if(data.products[i].name=== "Charles David") {
+        //         console.log("data");
+        //         console.log(data.products[i].name);
+        //     };
+        // };
+        //url to image
+         console.log(data.products[0].image.sizes);
+         console.log(data.products);
     }); 
 }); 
