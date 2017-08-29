07fc73b26433a9350a3c433a0087a93d645e122b
mc4tech <chavezmeguel@yahoo.com>
2017-08-28 23:57:04 -0400

    for loop for the response to get img to retailer link

--- models/shopping.js
+++ models/shopping.js
@@ -1,37 +1,40 @@
-// get walking directions from central park to the empire state building
-var http = require("http");
-var authKey = "uid6676-39776974-43";
-var methodName = "brands";
-var url = "http://api.shopstyle.com/api/v2/products?pid=" + authKey + "&fts=men&offset=0&limit=10";
 
-// get is a simple wrapper for request()
-// which sets the http method to GET
-var request = http.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event    
-    var buffer = "", 
-        data,
-        route;
+module.exports = function(search) {
+    // get walking directions from central park to the empire state building
+    var http = require("http");
+    var authKey = "uid6676-39776974-43";
+    // var search = "men";
+    var url = "http://api.shopstyle.com/api/v2/products?pid=" + authKey + "&fts=" + search + "&offset=0&limit=10";
 
-    response.on("data", function (chunk) {
-        buffer += chunk;
+    // get is a simple wrapper for request()
+    // which sets the http method to GET
+    var request = http.get(url, function (response) {
+        // data is streamed in chunks from the server
+        // so we have to handle the "data" event    
+        var buffer = "", 
+            data,
+            route;
+
+        response.on("data", function (chunk) {
+            buffer += chunk;
+        }); 
+
+        response.on("end", function (err) {
+            // finished transferring data
+            // dump the raw data
+            // console.log(buffer);
+            console.log("\n");
+            data = JSON.parse(buffer);
+
+            for(var i = 0; i < data.products.length; i++) {
+                //loops through the JSON response and logs 
+                //img url, brandedName, price and link to retailer
+                console.log(data.products[i].image.sizes.Large.url);
+                console.log(data.products[i].brandedName);
+                console.log(data.products[i].priceLabel);
+                console.log(data.products[i].clickUrl);    
+            };
+           
+        }); 
     }); 
-
-    response.on("end", function (err) {
-        // finished transferring data
-        // dump the raw data
-        // console.log(buffer);
-        console.log("\n");
-        data = JSON.parse(buffer);
-        // route = data.routes[0];
-        // for(var i = 0; i < data.products.length; i++) {
-        //     if(data.products[i].name=== "Charles David") {
-        //         console.log("data");
-        //         console.log(data.products[i].name);
-        //     };
-        // };
-        //url to image
-         console.log(data.products[0].image.sizes);
-         console.log(data.products);
-    }); 
-}); 
+};
